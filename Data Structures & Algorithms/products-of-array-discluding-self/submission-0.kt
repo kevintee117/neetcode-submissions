c89643d603ff4103d1b1469b2a009f03e2206b50
Kevin Tee <ktee@purdue.edu>
2026-06-10 15:30:35 -0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.kt
@@ -0,0 +1,19 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        val res = IntArray(nums.size) { 1 }
+
+        var prefix = 1
+        for (i in nums.indices) {
+            res[i] = prefix
+            prefix *= nums[i]
+        }
+
+        var postfix = 1
+        for (i in nums.size - 1 downTo 0) {
+            res[i] *= postfix
+            postfix *= nums[i]
+        }
+
+        return res
+    }
+}
