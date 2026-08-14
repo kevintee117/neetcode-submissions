49e239730c834b714baf46743d5a2dd5b0a8b972
Kevin Tee <ktee@purdue.edu>
2026-08-14 15:31:26 -0700

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = "";
+    
+        for(let i = 0;i < strs.length; i++) {
+            result += strs[i].length + "#" + strs[i]
+        }
+        console.log(result)
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let res = [];
+        let i = 0;
+        while (i < str.length) {
+            let j = i;
+            while (str[j] !== '#') {
+                j++;
+            }
+            let length = parseInt(str.substring(i, j));
+            i = j + 1;
+            j = i + length;
+            res.push(str.substring(i, j));
+            i = j;
+        }
+        return res;
+
+    }
+}
